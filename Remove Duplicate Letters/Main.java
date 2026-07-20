6b908c6ef6cb95a0a5a2c4374f7e2ec5d606e9bf
alanruizc <alanjoseruizc@gmail.com>
2026-07-19 23:52:40 -0700

    refactor: rdl main class

--- Remove Duplicate Letters/Main.java
+++ Remove Duplicate Letters/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Solution1 sol = new Solution1();
-        System.out.println("Remove Duplicate Letters");
-        System.out.print("enter s(string): ");
+        System.out.print("Enter string: ");
         String s = in.nextLine();
         String result = sol.removeDuplicateLetters(s);
         System.out.println("input: " + s);
