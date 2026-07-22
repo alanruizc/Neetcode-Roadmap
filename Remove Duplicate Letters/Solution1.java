41835aa56813074a91cc5dc5b2544e4f64d9b1bb
alanruizc <alanjoseruizc@gmail.com>
2026-07-21 23:58:20 -0700

    refactor: solution1 w stackMask

--- Remove Duplicate Letters/Solution1.java
+++ Remove Duplicate Letters/Solution1.java
@@ -6,33 +6,27 @@ public class Solution1 {
         for (int i = 0; i < n; i++) {
             lastInd[charArray[i] - 'a'] = i;
         }
-
-        boolean[] inStack = new boolean[26];
-
+        int inStackMask = 0;
         char[] stack = new char[26];
         int top = 0;
-
         for (int i = 0; i < n; i++) {
             char c = charArray[i];
             int currIdx = c - 'a';
-
-            if (inStack[currIdx]) {
+            if ((inStackMask & (1 << currIdx)) != 0) {
                 continue;
             }
-
             while (top > 0) {
                 char topChar = stack[top - 1];
                 int topIdx = topChar - 'a';
-
                 if (c < topChar && i < lastInd[topIdx]) {
-                    inStack[topIdx] = false;
+                    inStackMask &= ~(1 << topIdx);
                     top--;
                 } else {
                     break;
                 }
             }
             stack[top++] = c;
-            inStack[currIdx] = true;
+            inStackMask |= (1 << currIdx);
         }
         return new String(stack, 0, top);
     }
