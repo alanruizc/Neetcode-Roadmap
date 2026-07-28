2042551fe3f0695eb6923dd81c7ce85e187e7444
alanruizc <alanjoseruizc@gmail.com>
2026-07-27 23:51:03 -0700

    refactor ibt solution

--- Invert Binary Tree/Solution1.java
+++ Invert Binary Tree/Solution1.java
@@ -1,14 +1,14 @@
 public class Solution1 {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
-            return root;
+            return null;
         }
 
-        TreeNode leftInv = invertTree(root.left);
-        TreeNode rightInv = invertTree(root.right);
-
-        root.left = rightInv;
-        root.right = leftInv;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
 
         return root;
     }
