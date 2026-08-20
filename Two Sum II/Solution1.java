f8b42fbef2d33bd459626929d57d4f95241d3352
alanruizc <alanjoseruizc@gmail.com>
2026-08-19 21:51:07 -0700

    refactor ts2 solution

--- Two Sum II/Solution1.java
+++ Two Sum II/Solution1.java
@@ -1,20 +1,19 @@
-public class Solution1 {
+public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int left = 0;
         int right = numbers.length - 1;
 
         while (left < right) {
-            int currentSum = numbers[left] + numbers[right];
-
-            if (currentSum == target) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
                 return new int[]{left + 1, right + 1};
-            } else if (currentSum < target) {
+            }
+            if (sum < target) {
                 left++;
             } else {
                 right--;
             }
         }
-
-        return new int[]{-1, -1};
+        return new int[0];
     }
 }
