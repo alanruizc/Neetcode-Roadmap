318fa77adc24017173bd705bc62c3661cfaa12ce
alanruizc <alanjoseruizc@gmail.com>
2026-08-25 23:58:42 -0700

    replace string sorting with 26-element char count hashing

--- Group Anagrams/Solution1.java
+++ Group Anagrams/Solution1.java
@@ -1,20 +1,18 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution1 {
     public List<List<String>> groupAnagrams(String[] strs) {
         if (strs == null || strs.length == 0) {
-            return Collections.emptyList();
+            return new ArrayList<>();
         }
 
         Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
-            char[] chars = s.toCharArray();
-            Arrays.sort(chars);
-            String key = new String(chars);
+            char[] count = new char[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            String key = String.valueOf(count);
 
             map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
