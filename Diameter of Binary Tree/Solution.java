7efe955382c4aea97f0a9c0fb6614ca52c445540
alanruizc <alanjoseruizc@gmail.com>
2026-08-31 23:57:51 -0700

    refactor: dbt solution class wo int max

--- Diameter of Binary Tree/Solution.java
+++ Diameter of Binary Tree/Solution.java
@@ -1,17 +1,21 @@
 public class Solution {
+    private int diameter = 0;
+
     public int diameterOfBinaryTree(TreeNode root) {
-        int[] max = new int[1];
-        maxDepth(root, max);
-        return max[0];
+        maxDepth(root);
+        return diameter;
     }
 
-    private int maxDepth(TreeNode node, int[] max) {
-        if (node == null) return 0;
+    private int maxDepth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
 
-        int left = maxDepth(node.left, max);
-        int right = maxDepth(node.right, max);
+        int leftDepth = maxDepth(node.left);
+        int rightDepth = maxDepth(node.right);
 
-        max[0] = Math.max(max[0], left + right);
-        return 1 + Math.max(left, right);
+        diameter = Math.max(diameter, leftDepth + rightDepth);
+
+        return Math.max(leftDepth, rightDepth) + 1;
     }
 }
